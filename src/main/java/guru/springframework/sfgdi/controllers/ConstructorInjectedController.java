1b690d2c7e0e4320c74b6f813911c3930741a356
Florin Ganea <florin.ganea@gmail.com>
2020-06-01 09:28:42 +0300

    Spring Dependency Injection - qualifiers.

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgdi.controllers;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import guru.springframework.sfgdi.services.GreetingService;
@@ -10,7 +11,7 @@ public class ConstructorInjectedController {
     
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorInjectedGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
     
